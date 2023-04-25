273ed0ded9d648c747db379d5eb4f3d250c4e657
Andres2902 <72894951+Andres2902@users.noreply.github.com>
2023-04-25 09:57:18 -0500

    loop
    ahora queda en bucle hasta que ingrese una respuesta incorrect

--- __init__.py
+++ __init__.py
@@ -6,21 +6,25 @@ def game():
         operation = int(input('Choice an operation: '))
         num_1 = eval(input('Enter first number: '))
         num_2 = eval(input('Enter second number: '))
-        answer = int(input('Enter you answer: ')   
-    if operation == 1:
-            result = suma(num_1, num_2)
-    if operation == 2:
-            result =resta(num_1, num_2)         
-    if operation == 3:
-            result =multiplicacion(num_1, num_2)                     
-    if operation == 4:                     
-            result =division(num_1, num_2)
+        answer = int(input('Enter you answer: '))   
+        if operation == 1:
+                result = suma(num_1, num_2)
+                
+        elif operation == 2:
+                result =resta(num_1, num_2)
+                       
+        elif operation == 3:
+                result =multiplicacion(num_1, num_2) 
+                                 
+        elif operation == 4:                 
+                result =division(num_1, num_2)
+                
         if result == answer:
             score += 1
             print('Correct!!')
         else:
             print('Incorrect')
-        
-        break
+            break
+
     print(f'======== Game Over ========\nYou score is {score}\nKeep going!')
 game()
