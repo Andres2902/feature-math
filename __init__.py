91f44f5d86712145364ebf0fc44e5ee1d89c9e74
Andres2902 <72894951+Andres2902@users.noreply.github.com>
2023-04-25 08:35:05 -0500

    se agregaron mas opciones
    las opciones de resta, multiplicación y división

--- __init__.py
+++ __init__.py
@@ -1,21 +1,20 @@
 from math_game import*
-  def game():
+def game():
     score = 0
-  while True:
-  print('======== Menu ======== \n 1.Add \n 2.Resta \n 3.Multiplicación \n 4. División")
-  operation = int(input('Choice an operation:
-'))
-  num_1 = input('Enter first number: ')
-  num_2 = input('Enter second number: ')
-  answer = int(input('Enter you answer: '))
-  if operation == 1:
-result = add(num_1, num_2)
-  if result == answer:
-    score += 1
-print('Correct!!')
-  else:
-    print('Incorrect')
-  break
-    print(f'======== Game Over ========\nYou score
-  is {score}\nKeep going!')
-    game()
+    while True:
+        print("======== Menu ======== \n 1.Add \n 2.Resta \n 3.Multiplicación \n 4. División")
+        operation = int(input('Choice an operation: '))
+        num_1 = eval(input('Enter first number: '))
+        num_2 = eval(input('Enter second number: '))
+        answer = int(input('Enter you answer: '))
+        if operation == 1:
+            result = add(num_1, num_2)
+        if result == answer:
+            score += 1
+            print('Correct!!')
+        else:
+            print('Incorrect')
+        
+        break
+    print(f'======== Game Over ========\nYou score is {score}\nKeep going!')
+game()
