d4bf783e3f8a6c1c9eaf5e9b800632886f21a7b3
Andres2902 <72894951+Andres2902@users.noreply.github.com>
2023-04-25 08:21:53 -0500

    multiplicación
    se agregó la función de multiplicación

--- math_game.py
+++ math_game.py
@@ -3,14 +3,17 @@ def add(num_1, num_2):
     print(f'{num_1} + {num_2} es igual a {result}')
     return result
 
-
 def resta(num_1, num_2):
     result = num_1 - num_2
     print(f'{num_1} - {num_2} es igual a {result}')
     return result
 
-
 def multiplicacion(num_1, num_2):
     result=num_1 * num_2
     print(f'{num_1} * {num_2} es igual a {result}')
+    return result
+
+def division(num_1, num_2):
+    result=num_1 / num_2
+    print(f'{num_1} / {num_2} es igual a {result}')
     return result
