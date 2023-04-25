0aceb132a1954347b8be953611dde003646cf9cc
J-p-M77 <131775741+J-p-M77@users.noreply.github.com>
2023-04-25 08:20:31 -0500

    Update math_game.py

--- math_game.py
+++ math_game.py
@@ -2,3 +2,15 @@ def add(num_1, num_2):
     result = num_1 + num_2
     print(f'{num_1} + {num_2} es igual a {result}')
     return result
+
+
+def resta(num_1, num_2):
+    result = num_1 - num_2
+    print(f'{num_1} - {num_2} es igual a {result}')
+    return result
+
+
+def multiplicacion(num_1, num_2):
+    result=num_1 * num_2
+    print(f'{num_1} * {num_2} es igual a {result}')
+    return result
