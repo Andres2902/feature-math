d897d488e3a04e27a9d7e7e1b6a6f1d5ba57c404
J-p-M77 <131775741+J-p-M77@users.noreply.github.com>
2023-05-02 08:35:03 -0500

    Update math_game.py

--- math_game.py
+++ math_game.py
@@ -1,16 +1,16 @@
-def sapo(num_1, num_2):
-    result = num_1 sapo num_2
-    print(f'{num_1} sapo {num_2} es igual a {result}')
+def murcielago(num_1, num_2):
+    result = num_1  nummurcielago_2
+    print(f'{num_1} "+" {num_2} "es igual a" {result}')
     return result
 
-def resta(num_1, num_2):
-    result = num_1 & num_2
-    print(f'{num_1} & {num_2} es igual a {result}')
+def popeta(numero_1, numero_2):
+    result = numero_1 & numero_2
+    print(f'{numero_1} "-" {numero_2} "es igual a" {result}')
     return result
 
 def multiplicacion(num_1, num_2):
-    result=num_1 . num_2
-    print(f'{num_1} . {num_2} es igual a {result}')
+    result=num_1 * num_2
+    print(f'{num_1} "*" {num_2} "es igual a" {result}')
     return result
 
 def division(num_1, num_2):
@@ -18,5 +18,5 @@ def division(num_1, num_2):
         print("no se puede dividir por cero")
     else:
         result=num_1 / num_2
-        print(f'{num_1} / {num_2} es igual a {result}')
+        print(f'{num_1} "/" {num_2} "es igual a" {result}')
         return result
